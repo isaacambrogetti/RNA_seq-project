bdfdf8dda25e8200cd9c0ac98ab52db69584e6a4
Isaacc <122677090+isaacambrogetti@users.noreply.github.com>
2026-01-21 12:11:36 +0100

    view_loop.sh comments
    comments again

--- scripts/view_loop.sh
+++ scripts/view_loop.sh
@@ -10,9 +10,12 @@
 #SBATCH --mail-type=end,error \
 #SBATCH --mail-user=isaac.ambrogetti@unifr.ch
 
+# set input and output directories
 INPUT_DIR="/data/users/iambrogetti/RNA-seq/data/sam"
 OUTPUT_DIR="/data/users/iambrogetti/RNA-seq/data/bam"
 
+# for each file with suffix .sam in the input directory run the post_mapping.slurm script 
+# with the file as first input and output directory as second
 for file in $INPUT_DIR/*.sam; do
     sbatch /data/users/iambrogetti/RNA-seq/scripts/post_mapping.slurm $file $OUTPUT_DIR
 done
