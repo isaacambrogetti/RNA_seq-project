b3e6de108bb48efb2b0ec0195b67aa9f01e21bc6
Isaacc <122677090+isaacambrogetti@users.noreply.github.com>
2026-01-21 12:07:36 +0100

    reads_per_gene.sh comments
    Added comments for clarity

--- scripts/reads_per_gene.sh
+++ scripts/reads_per_gene.sh
@@ -10,7 +10,7 @@
 #SBATCH --mail-type=end,error \
 #SBATCH --mail-user=isaac.ambrogetti@unifr.ch
 
-
+# set directories for containers, genome file, input bams, output file, temporary directory and multiqc output
 CONTAINER_feature="/containers/apptainer/subread_2.0.6.sif"
 CONTAINER_multiqc="/containers/apptainer/multiqc-1.19.sif"
 GENOME_FILES="/data/users/iambrogetti/RNA-seq/data/ref_genome/Mus_musculus.GRCm39.115.gtf"
@@ -19,14 +19,15 @@ OUTPUT_FILE="/data/users/iambrogetti/RNA-seq/data/bam/reads_gene_counts.txt"
 TMP="/data/users/iambrogetti/RNA-seq/.tmp"
 QC_FILE="/data/users/iambrogetti/RNA-seq/data/bam"
 
-
+# run the featureCounts command to create the table
 apptainer exec $CONTAINER_feature featureCounts \
     -a $GENOME_FILES -o $OUTPUT_FILE \
     -p -B -C -s 2 --tmpDir $TMP \
     -T $SLURM_CPUS_PER_TASK $INPUT_FILES
 
-sed -E -i 's#[^[:space:]]*/(SRR[0-9]+)\_sorted\.bam#\1#g' $OUTPUT_FILE # clear the col names to just the SRC name
-
+# clear the col names to just the SRC name for the .txt file and the .txt.summary
+sed -E -i 's#[^[:space:]]*/(SRR[0-9]+)\_sorted\.bam#\1#g' $OUTPUT_FILE 
 sed -E -i 's#[^[:space:]]*/(SRR[0-9]+)\_sorted\.bam#\1#g' ${OUTPUT_FILE}.summary
 
+# run multiqc on summary file
 apptainer exec ${CONTAINER_multiqc} multiqc ${QC_FILE}/*.summary -o ${QC_FILE}
